120461c3eaeb99a374ced8e474364638b07d842b
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-11-24 16:16:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,8 +15,8 @@ aria2c --allow-overwrite=true --auto-file-renaming=true --bt-enable-lpd=true --b
 
 
 ###### Adding Files ######
-wget -O /app/tobrot/aria2/dht.dat https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
-wget -O /app/tobrot/aria2/dht6.dat https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
+wget -O tobrot/aria2/dht.dat https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
+wget -O tobrot/aria2/dht6.dat https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
 TRACKER=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt -: https://ngosang.github.io/trackerslist/trackers_all_http.txt -: https://newtrackon.com/api/all -: https://raw.githubusercontent.com/DeSireFire/animeTrackerList/master/AT_all.txt -: https://torrends.to/torrent-tracker-list/?download=latest | awk '$1' | tr '\n' ',' | cat`
 ran=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
 ###### Done Addding Files ######
